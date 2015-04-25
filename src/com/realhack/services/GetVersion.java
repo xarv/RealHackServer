885b80313218155acb462e019569f540f3a0d827
Harshit <harsht91@gmail.com>
2015-04-25 14:11:05 +0530

    initial commit

---
+++ src/com/realhack/services/GetVersion.java
@@ -0,0 +1,5 @@
+package com.realhack.services;
+
+public class GetVersion {
+
+}
---
+++ src/com/realhack/servlets/RequestHandlerServlet.java
@@ -0,0 +1,38 @@
+package com.realhack.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestHandlerServlet
+ */
+public class RequestHandlerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RequestHandlerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().println("Hello World");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
