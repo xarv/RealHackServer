068695f1f1707b21411c118cf78a4d809ce4d544
Harshit <harsht91@gmail.com>
2015-04-25 15:30:35 +0530

    including jersey

--- src/com/realhack/services/GetVersion.java
+++ src/com/realhack/services/GetVersion.java
@@ -1,5 +1,30 @@
 package com.realhack.services;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+
+@Path("/getversion")
 public class GetVersion {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello() {
+		return "Hello World RESTful Jersey!";
+	}
 
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public String sayXMLHello() {
+		return "<?xml version=\"1.0\"?>" + "<hello> Hello World RESTful Jersey"
+				+ "</hello>";
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String sayHtmlHello() {
+		return "<html> " + "<title>" + "Hello World RESTful Jersey"
+				+ "</title>" + "<body><h1>" + "Hello World RESTful Jersey"
+				+ "</body></h1>" + "</html> ";
+	}
 }
